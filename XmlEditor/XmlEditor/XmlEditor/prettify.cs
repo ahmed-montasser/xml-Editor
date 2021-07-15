8bd7a0e3162fd0a60d4fb2cb71a8965aae28426a
MedoFahmy <74153862+MedoFahmy@users.noreply.github.com>
2021-07-15 22:43:15 +0200

    Prettifying Edits

--- XmlEditor/XmlEditor/XmlEditor/prettify.cs
+++ XmlEditor/XmlEditor/XmlEditor/prettify.cs
@@ -18,10 +18,67 @@ namespace XmlEditor
                     return n;
             }
         }
+        private List<string> lines(List<string> xml)
+        {
+            List<string> xml_e = new List<string>();
+            for (int i = 0; i < xml.Count(); i++)
+            {
+                string a = "", b = "";
+                string x = xml[i];
+                int m;
+                for (m = 0; m < x.Length; m++)
+                {
+                    if (x[m] != ' ')
+                    {
+                        if (x[m] == '<')
+                        {
+                            a = x.Substring(m, x.IndexOf('>') - m + 1);
+                            b = x.Substring(x.IndexOf('>') + 1, x.Length - x.IndexOf('>') - 1);
+                            break;
+                        }
+                        else
+                        {
+                            if (x.Contains('<'))
+                            {
+                                a = x.Substring(m, x.IndexOf('<') - m + 1);
+                                b = x.Substring(x.IndexOf('<'), x.Length - x.IndexOf('<') - 1);
+                            }
+                            else
+                            {
+                                a = x;
+                            }
+                            break;
+                        }
+                    }
+                }
+                for (int n = 0; n < i; n++)
+                {
+                    xml_e.Add(xml[n]);
+                }
+                xml_e.Add(a);
+                if (b != "")
+                {
+                    i++;
+                    xml_e.Add(b);
+                }
+                for (int n = i + 1; n < xml.Count(); n++)
+                {
+                    xml_e.Add(xml[n]);
+                }
+                xml.Clear();
+                for (int n = 0; n < xml_e.Count(); n++)
+                {
+                    xml.Add(xml_e[n]);
+                }
+                xml_e.Clear();
+            }
+            return xml;
+        }
         public List<string> operation2(List<string> xml)
         {
-            int spaces=0;
-            for(int i=0;i<xml.Count();i++)
+            List<string> xml_e = lines(xml);
+            int spaces = 0;
+            for (int i = 0; i < xml.Count(); i++)
             {
                 if (xml[i].Contains("</"))
                 {
@@ -30,8 +87,8 @@ namespace XmlEditor
                         spaces--;
                     }
                 }
-                for(int j=0;j<spaces;j++)
-                {xml[i]=xml[i].Insert(0,"    ");}
+                for (int j = 0; j < spaces; j++)
+                { xml[i] = xml[i].Insert(0, "    "); }
                 if (xml[i].Contains('<') && !xml[i].Contains("</"))
                 {
                     if (brackets_number(xml[i]) != 2)
